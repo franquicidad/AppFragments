27f21978f5ed927689585e0dd450d83c2be6d930
mac <franquicidad@gmail.com>
2017-07-27 17:19:43 -0500

    Added Fragments new classes

--- app/src/main/java/com/example/mac/appfragments/SoldeNapoles.java
+++ app/src/main/java/com/example/mac/appfragments/SoldeNapoles.java
@@ -1,8 +1,15 @@
 package com.example.mac.appfragments;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by mac on 26/07/17.
@@ -14,5 +21,7 @@ public class SoldeNapoles extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sol_de_napoles);
+
+
     }
 }
